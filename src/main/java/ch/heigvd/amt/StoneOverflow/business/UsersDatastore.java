1c2786c027720ae5f59dab01f7941cdecbc87023
Robin Müller <33422394+AziXus@users.noreply.github.com>
2020-09-25 18:44:07 +0200

    Implemented BCrypt in user datastore

--- src/main/java/ch/heigvd/amt/StoneOverflow/business/UsersDatastore.java
+++ src/main/java/ch/heigvd/amt/StoneOverflow/business/UsersDatastore.java
@@ -4,6 +4,7 @@ import ch.heigvd.amt.StoneOverflow.domain.LoginCommand;
 import ch.heigvd.amt.StoneOverflow.domain.RegisterCommand;
 
 import ch.heigvd.amt.StoneOverflow.domain.User;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 
 import javax.ejb.Singleton;
 import java.util.HashMap;
@@ -11,19 +12,21 @@ import java.util.HashMap;
 @Singleton
 public class UsersDatastore {
     // Create a HashMap object called capitalCities
-    private HashMap<String, User> users = new HashMap<String, User>(){{put("test@test.com", User.builder().username("test@test.com").password("test").build());}};
+    private HashMap<String, User> users = new HashMap<String, User>(){{put("test@test.com", User.builder().username("test@test.com").password(BCrypt.hashpw("test", BCrypt.gensalt())).build());}};
 
     public void addUser(RegisterCommand register){
+        String hashedPassword = BCrypt.hashpw(register.getPassword(), BCrypt.gensalt());
         User newUser = User.builder()
                 .username(register.getUsername())
-                .password(register.getPassword())
+                .password(hashedPassword)
                 .build();
         users.put(register.getUsername(), newUser);
     }
 
     public boolean isValidUser(LoginCommand login){
-        return users.containsKey(login.getUsername())
-                && users.get(login.getUsername()).getPassword().equals(login.getPassword());
+        User user = users.get(login.getUsername());
+        return user != null
+                && BCrypt.checkpw(login.getPassword(), user.getPassword());
     }
 
     public boolean isUserIn(LoginCommand login){
