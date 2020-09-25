72a40c15f8b2f6569738d4b3c69085f7bd11772e
Robin Müller <33422394+AziXus@users.noreply.github.com>
2020-09-25 21:10:44 +0200

    Refactored InMemoryRepository into super class

--- src/main/java/ch/heigvd/amt/StoneOverflow/infrastructure/persistance/memory/InMemoryQuestionRepository.java
+++ src/main/java/ch/heigvd/amt/StoneOverflow/infrastructure/persistance/memory/InMemoryQuestionRepository.java
@@ -5,51 +5,14 @@ import ch.heigvd.amt.StoneOverflow.application.Question.QuestionQuery;
 import ch.heigvd.amt.StoneOverflow.domain.Question.IQuestionRepository;
 import ch.heigvd.amt.StoneOverflow.domain.Question.Question;
 import ch.heigvd.amt.StoneOverflow.domain.Question.QuestionId;
-import ch.heigvd.amt.StoneOverflow.domain.Question.QuestionType;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.Collection;
-import java.util.Map;
-import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.stream.Collectors;
 
-public class InMemoryQuestionRepository implements IQuestionRepository {
-   private Map<QuestionId, Question> store = new ConcurrentHashMap<>();
-
-   @Override
-   public void save(Question question){
-       store.put(question.getId(), question);
-   }
-
-    @Override
-    public void remove(QuestionId questionId) {
-        store.remove(questionId);
-    }
-
-    @Override
-    public Optional<Question> findById(QuestionId questionId) {
-        Question existingQuestion = store.get(questionId);
-        if (existingQuestion == null){
-            return Optional.empty();
-        }
-        Question question = existingQuestion.toBuilder().build();
-        return Optional.of(question);
-    }
-
-    @Override
-    public Collection<Question> findAll() {
-        return store.values().stream()
-                .map(question -> question.toBuilder().build())
-                .collect(Collectors.toList());
-    }
-
+public class InMemoryQuestionRepository extends InMemoryRepository<Question, QuestionId> implements IQuestionRepository {
     @Override
     public Collection<Question> find(QuestionQuery questionQuery) {
-        if(questionQuery != null && questionQuery.isSqlSearch()){
-            return findAll().stream()
-                    .filter(question -> question.getQuestionType() == QuestionType.SQL)
-                    .collect(Collectors.toList());
-        }
-        return findAll();
+        //todo: implement question queries
+        throw new NotImplementedException();
     }
 }
