2320c083de1cb4ef52585e1cb6f7fc4dbf796191
Robin Müller <33422394+AziXus@users.noreply.github.com>
2020-09-25 22:21:39 +0200

    Register IdentityManagementFacade in ServiceRegistry

--- src/main/java/ch/heigvd/amt/StoneOverflow/application/ServiceRegistry.java
+++ src/main/java/ch/heigvd/amt/StoneOverflow/application/ServiceRegistry.java
@@ -1,14 +1,19 @@
 package ch.heigvd.amt.StoneOverflow.application;
 
 import ch.heigvd.amt.StoneOverflow.application.Question.QuestionFacade;
+import ch.heigvd.amt.StoneOverflow.application.identitymgmt.IdentityManagementFacade;
 import ch.heigvd.amt.StoneOverflow.domain.Question.IQuestionRepository;
+import ch.heigvd.amt.StoneOverflow.domain.user.IUserRepository;
 import ch.heigvd.amt.StoneOverflow.infrastructure.persistance.memory.InMemoryQuestionRepository;
+import ch.heigvd.amt.StoneOverflow.infrastructure.persistance.memory.InMemoryUserRepository;
 
 public class ServiceRegistry {
     private static ServiceRegistry singleton;
 
     private static IQuestionRepository questionRepository;
+    private static IUserRepository userRepository;
     private static QuestionFacade questionFacade;
+    private static IdentityManagementFacade identityManagementFacade;
 
     public static ServiceRegistry getServiceRegistry(){
         if (singleton == null){
@@ -20,10 +25,16 @@ public class ServiceRegistry {
     private ServiceRegistry(){
         singleton = this;
         questionRepository = new InMemoryQuestionRepository();
+        userRepository = new InMemoryUserRepository();
         questionFacade = new QuestionFacade(questionRepository);
+        identityManagementFacade = new IdentityManagementFacade(userRepository);
     }
 
     public QuestionFacade getQuestionFacade() {
         return questionFacade;
+    }
+
+    public IdentityManagementFacade getIdentityManagementFacade() {
+        return identityManagementFacade;
     }
 }
