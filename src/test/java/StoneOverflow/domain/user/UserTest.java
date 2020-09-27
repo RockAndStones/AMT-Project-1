f6d0636106388853f914672af92ce36a9fa127da
Robin Müller <33422394+AziXus@users.noreply.github.com>
2020-09-27 12:15:17 +0200

    Added unit test to some classes

---
+++ src/test/java/StoneOverflow/domain/user/UserTest.java
@@ -0,0 +1,80 @@
+package StoneOverflow.domain.user;
+
+import ch.heigvd.amt.StoneOverflow.domain.user.User;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserTest {
+    @Test
+    public void deepCloneCreatesNewObject() {
+        User u = User.builder()
+                .username("test")
+                .email("test@test.com")
+                .firstName("First name")
+                .lastName("Last name")
+                .plaintextPassword("pass")
+                .build();
+        User u2 = u.deepClone();
+
+        assertEquals(u, u2);
+        assertNotSame(u, u2);
+    }
+
+    @Test
+    public void userIdShouldBeAutomaticallGenerated() {
+        User u = User.builder()
+                .username("test")
+                .email("test@test.com")
+                .firstName("First name")
+                .lastName("Last name")
+                .plaintextPassword("pass")
+                .build();
+
+        assertNotNull(u.getId());
+    }
+
+    @Test
+    public void userPasswordShouldBeHashed() {
+        String password = "pass";
+
+        User u = User.builder()
+                .username("test")
+                .email("test@test.com")
+                .firstName("First name")
+                .lastName("Last name")
+                .plaintextPassword(password)
+                .build();
+
+        assertNotEquals(password, u.getHashedPassword());
+    }
+
+    @Test
+    public void userShouldAuthenticateWithCorrectPassword() {
+        String password = "pass";
+
+        User u = User.builder()
+                .username("test")
+                .email("test@test.com")
+                .firstName("First name")
+                .lastName("Last name")
+                .plaintextPassword(password)
+                .build();
+
+        assertTrue(u.authenticate(password));
+    }
+
+    @Test
+    public void userShouldNotAuthenticateWithIncorrectPassword() {
+        User u = User.builder()
+                .username("test")
+                .email("test@test.com")
+                .firstName("First name")
+                .lastName("Last name")
+                .plaintextPassword("pass")
+                .build();
+
+        assertFalse(u.authenticate("Different password"));
+    }
+}
