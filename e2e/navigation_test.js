ec4e6be9ba751c959af7d39ef52f191bc429d62e
bruno <egremy.bruno@gmail.com>
2020-09-30 14:42:24 +0200

    WORK IN PROGRESS Made used of actors, pageObjects and pageFragments. Is tested :  - Login success  - Login fail  - Navigation from home page to login page  - Navigation from login page to home page

--- e2e/navigation_test.js
+++ e2e/navigation_test.js
@@ -1,13 +1,16 @@
+const {homePage, loginPage } = inject();
+
 Feature('Navigation');
 
-Scenario('From home to login', (I) => {
-  I.amOnPage('http://localhost:8080/StoneOverflow-1.0-SNAPSHOT/home');
-  I.clickLink('Log in/Sign up'); // Upper nav bar link
-  I.seeElement("//html/body/div/div[1]/div[2]/form/input"); // "Log In" button on login page
+Scenario('Navigate from home page to login page', (I) => {
+  I.amOnPage('home');
+  homePage.goToLoginPage();
+  I.seeInCurrentUrl('login');
 });
 
-Scenario('From login to home', (I) => {
-  I.amOnPage('http://localhost:8080/StoneOverflow-1.0-SNAPSHOT/login');
-  I.clickLink('StoneOverflow'); // Upper left "StoneOverflow" logo
-  I.see("Questions"); // In-page title, main content
+Scenario('Navigate from login page to home page', (I) => {
+  I.amOnPage('login');
+  loginPage.goToHomePage();
+  I.seeInCurrentUrl('home');
 });
+
