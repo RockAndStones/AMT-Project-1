de48735ee71466f6af9482cb8040523d9b7195c3
Robin Müller <33422394+AziXus@users.noreply.github.com>
2020-10-02 17:14:00 +0200

    Implemented JdbcUserRepository.findAll function

--- src/main/java/ch/heigvd/amt/StoneOverflow/infrastructure/persistance/jdbc/JdbcUserRepository.java
+++ src/main/java/ch/heigvd/amt/StoneOverflow/infrastructure/persistance/jdbc/JdbcUserRepository.java
@@ -45,6 +45,31 @@ public class JdbcUserRepository implements IUserRepository {
 
     @Override
     public Collection<User> findAll() {
+        Collection<User> users = new LinkedList<>();
 
+        try {
+            Connection con = dataSource.getConnection();
+
+            PreparedStatement ps = con.prepareStatement("SELECT id, username, mail, firstName, lastName, password FROM User");
+            ResultSet rs = ps.executeQuery();
+
+            while(rs.next()) {
+                User u = User.builder()
+                        .id(new UserId(rs.getString("id")))
+                        .username(rs.getString("username"))
+                        .email(rs.getString("mail"))
+                        .firstName(rs.getString("firstName"))
+                        .lastName(rs.getString("lastName"))
+                        .hashedPassword(rs.getString("password"))
+                        .build();
+                users.add(u);
+            }
+            ps.close();
+            con.close();
+        } catch (SQLException ex) {
+            //todo: log/handle error
+        }
+
+        return users;
     }
 }
