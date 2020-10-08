78e2a51e2b38355fbc522ee18505a4cb5aebc638
b3cauda <arthur.becaud@heig-vd.ch>
2020-10-08 18:21:04 +0200

    Fix '/' urlPattern in QuestionQueryServlet

--- src/main/java/ch/heigvd/amt/StoneOverflow/ui/web/Question/QuestionQueryServlet.java
+++ src/main/java/ch/heigvd/amt/StoneOverflow/ui/web/Question/QuestionQueryServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name="QuestionsPageServlet", urlPatterns =  {"/","/home"})
+@WebServlet(name="QuestionsPageServlet", urlPatterns =  {"", "/home"})
 public class QuestionQueryServlet extends HttpServlet {
     private ServiceRegistry serviceRegistry;
     private QuestionFacade questionFacade;
