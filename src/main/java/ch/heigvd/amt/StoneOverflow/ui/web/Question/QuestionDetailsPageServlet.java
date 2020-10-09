82e6c5032fb75daf340c27d88d173a9c01c05d48
b3cauda <arthur.becaud@heig-vd.ch>
2020-10-09 14:01:39 +0200

    Fix Servlet name

--- src/main/java/ch/heigvd/amt/StoneOverflow/ui/web/Question/QuestionDetailsPageServlet.java
+++ src/main/java/ch/heigvd/amt/StoneOverflow/ui/web/Question/QuestionDetailsPageServlet.java
@@ -1,8 +1,5 @@
 package ch.heigvd.amt.StoneOverflow.ui.web.Question;
 
-import ch.heigvd.amt.StoneOverflow.application.Question.QuestionFacade;
-import ch.heigvd.amt.StoneOverflow.application.ServiceRegistry;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,19 +7,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "addQuestionServlet", urlPatterns = "/questionDetails")
+@WebServlet(name = "questionDetailsServlet", urlPatterns = "/questionDetails")
 public class QuestionDetailsPageServlet extends HttpServlet {
-
-    private ServiceRegistry serviceRegistry;
-    private QuestionFacade questionFacade;
-
-    @Override
-    public void init() throws ServletException {
-        super.init();
-        serviceRegistry = ServiceRegistry.getServiceRegistry();
-        questionFacade  = serviceRegistry.getQuestionFacade();
-    }
-
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.getRequestDispatcher("WEB-INF/views/questionDetails.jsp").forward(req, resp);
     }
