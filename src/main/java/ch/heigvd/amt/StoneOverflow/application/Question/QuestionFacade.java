b879d1e28e8e089ac12ac95112bd5b6088a9870e
Robin Müller <33422394+AziXus@users.noreply.github.com>
2020-10-12 13:03:43 +0200

    Updated question facade to sort by date

--- src/main/java/ch/heigvd/amt/StoneOverflow/application/Question/QuestionFacade.java
+++ src/main/java/ch/heigvd/amt/StoneOverflow/application/Question/QuestionFacade.java
@@ -4,6 +4,7 @@ import ch.heigvd.amt.StoneOverflow.domain.Question.IQuestionRepository;
 import ch.heigvd.amt.StoneOverflow.domain.Question.Question;
 
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -28,13 +29,16 @@ public class QuestionFacade {
     public QuestionsDTO getQuestions(QuestionQuery query){
         Collection<Question> allQuestions = questionRepository.find(query);
 
-        List<QuestionsDTO.QuestionDTO> allQuestionsDTO = allQuestions.stream().map(question -> QuestionsDTO.QuestionDTO.builder()
-                .title(question.getTitle())
-                .creator(question.getCreator())
-                .description(question.getDescription())
-                .nbVotes(question.getNbVotes())
-                .date(question.getDate())
-                .type(question.getQuestionType()).build()).collect(Collectors.toList());
+        List<QuestionsDTO.QuestionDTO> allQuestionsDTO = allQuestions.stream()
+                .sorted(Comparator.comparing(Question::getDate).reversed())
+                .map(question -> QuestionsDTO.QuestionDTO.builder()
+                    .title(question.getTitle())
+                    .creator(question.getCreator())
+                    .description(question.getDescription())
+                    .nbVotes(question.getNbVotes())
+                    .date(question.getDate())
+                    .type(question.getQuestionType()).build())
+                .collect(Collectors.toList());
 
         return QuestionsDTO.builder()
                 .questions(allQuestionsDTO)
