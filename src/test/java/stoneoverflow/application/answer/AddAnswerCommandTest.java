9438e14f098ee6948383f6f9100e57ad02b95485
b3cauda <arthur.becaud@heig-vd.ch>
2020-10-13 13:37:01 +0200

    New Answer/Comment application tests

---
+++ src/test/java/stoneoverflow/application/answer/AddAnswerCommandTest.java
@@ -0,0 +1,20 @@
+package stoneoverflow.application.answer;
+
+import ch.heigvd.amt.stoneoverflow.application.answer.AddAnswerCommand;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class AddAnswerCommandTest {
+    @Test
+    public void shouldSendDefaultValues() {
+        AddAnswerCommand addQuestionCommand = AddAnswerCommand.builder().build();
+
+        assertNull(addQuestionCommand.getAnswerTo());
+        assertEquals(addQuestionCommand.getDescription(), "No content");
+        assertEquals(addQuestionCommand.getCreator(), "Anonymous");
+        assertEquals(addQuestionCommand.getNbVotes(), 0);
+        assertNull(addQuestionCommand.getDate());
+    }
+}
