88719e1e07b2e2e0d7e26bb2a9814fe7ca80d5db
Naludrag <stephanecarvalho81@gmail.com>
2020-10-30 14:47:16 +0100

    Updated Scripts & resolved head title error in the table of the statistic page

--- run_unit_intergration_tests.sh
+++ run_unit_intergration_tests.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 mvn liberty:stop
+cd docker
+docker-compose up -d sql
+cd ..
 mvn clean package
 mvn liberty:create liberty:install-feature liberty:deploy
 mvn liberty:start
 curl http://localhost:8080/home
 mvn verify
+cd docker
+docker-compose down
 read -p "Press any key to quit ..."
 
