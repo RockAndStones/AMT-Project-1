3c0c0a1d83fef1684ceda84edf212d5ffbbd7ab8
Naludrag <stephanecarvalho81@gmail.com>
2020-10-30 13:34:42 +0100

    Updated run_unit_integration_tests.sh

--- run_unit_intergration_tests.sh
+++ run_unit_intergration_tests.sh
@@ -3,6 +3,7 @@ mvn liberty:stop
 mvn clean package
 mvn liberty:create liberty:install-feature liberty:deploy
 mvn liberty:start
+curl http://localhost:8080/home
 mvn verify
 read -p "Press any key to quit ..."
 
