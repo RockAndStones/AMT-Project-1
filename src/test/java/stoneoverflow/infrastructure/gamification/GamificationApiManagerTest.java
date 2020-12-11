2c958a51fb97517d4c7037ed340719174ede40c4
Robin Müller <33422394+AziXus@users.noreply.github.com>
2020-12-11 16:33:37 +0100

    Added sample test for the api

--- src/test/java/stoneoverflow/infrastructure/gamification/GamificationApiManagerTest.java
+++ src/test/java/stoneoverflow/infrastructure/gamification/GamificationApiManagerTest.java
@@ -1,20 +1,45 @@
 package stoneoverflow.infrastructure.gamification;
 
-import ch.heigvd.amt.stoneoverflow.infrastructure.gamification.Badge;
+import ch.heigvd.amt.gamification.ApiException;
+import ch.heigvd.amt.gamification.ApiResponse;
+import ch.heigvd.amt.gamification.api.DefaultApi;
+import ch.heigvd.amt.gamification.api.dto.NewApplication;
 import ch.heigvd.amt.stoneoverflow.infrastructure.gamification.GamificationApiManager;
 import org.apache.commons.lang3.RandomStringUtils;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
 
-import java.util.List;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.TimeZone;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GamificationApiManagerTest {
     GamificationApiManager gApiManager = new GamificationApiManager("unitTests-" +  RandomStringUtils.random(10, true, true));
 
-    @Test
-    public void shouldCreateBadge(){
-        assertEquals(201, gApiManager.createBadge(  RandomStringUtils.random(10, true, true),
-                                                            RandomStringUtils.random(10, true, true)));
+    private static final DefaultApi api = new DefaultApi();
+
+    @BeforeAll
+    static void initApi() throws IOException {
+        Properties properties = new Properties();
+        properties.load(GamificationApiManagerTest.class.getClassLoader().getResourceAsStream("environment.properties"));
+        String url = properties.getProperty("ch.heigvd.amt.gamification.server.url");
+        api.getApiClient().setBasePath(url);
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
     }
+
+    @Test
+    public void testApi() throws ApiException {
+        NewApplication app = new NewApplication();
+        app.setName(RandomStringUtils.random(10, true, true));
+        ApiResponse<Void> resp = api.createApplicationWithHttpInfo(app);
+        assertEquals(201, resp.getStatusCode());
+    }
+
+//    @Test
+//    public void shouldCreateBadge(){
+//        assertEquals(201, gApiManager.createBadge(  RandomStringUtils.random(10, true, true),
+//                                                            RandomStringUtils.random(10, true, true)));
+//    }
 }
