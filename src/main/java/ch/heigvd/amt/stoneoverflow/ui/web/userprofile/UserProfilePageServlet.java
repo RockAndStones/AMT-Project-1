e05200cfb58e27f09693ed25df5b176bd2053e5d
b3cauda <arthur.becaud@heig-vd.ch>
2021-01-03 21:42:19 +0100

    integrate badges visual in user profile page

--- src/main/java/ch/heigvd/amt/stoneoverflow/ui/web/userprofile/UserProfilePageServlet.java
+++ src/main/java/ch/heigvd/amt/stoneoverflow/ui/web/userprofile/UserProfilePageServlet.java
@@ -1,5 +1,11 @@
 package ch.heigvd.amt.stoneoverflow.ui.web.userprofile;
 
+import ch.heigvd.amt.gamification.api.dto.UserInfo;
+import ch.heigvd.amt.stoneoverflow.application.ServiceRegistry;
+import ch.heigvd.amt.stoneoverflow.application.gamification.GamificationFacade;
+import ch.heigvd.amt.stoneoverflow.application.identitymgmt.login.AuthenticatedUserDTO;
+
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,14 +15,34 @@ import java.io.IOException;
 
 @WebServlet(name = "UserProfilePage", urlPatterns = "/profile")
 public class UserProfilePageServlet extends HttpServlet {
+    @Inject
+    ServiceRegistry serviceRegistry;
+    private GamificationFacade gamificationFacade;
+
+    @Override
+    public void init() throws ServletException {
+        gamificationFacade = serviceRegistry.getGamificationFacade();
+    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String errorMessage = (String)request.getSession().getAttribute("errorMessage");
+        AuthenticatedUserDTO user = (AuthenticatedUserDTO) request.getSession().getAttribute("authenticatedUser");
+        String errorMessage = (String) request.getSession().getAttribute("errorMessage");
         request.getSession().removeAttribute("errorMessage");
 
         String message = (String)request.getSession().getAttribute("message");
         request.getSession().removeAttribute("message");
+
+        System.out.println(user.getId().asString());
+
+        UserInfo userInfo = gamificationFacade.getUserInfo(user.getId().asString());
+        if (userInfo == null) {
+            userInfo = new UserInfo();
+            request.setAttribute("isGamificationOn", false);
+        } else request.setAttribute("isGamificationOn", true);
+        request.setAttribute("userGameInfo", userInfo);
+
+        System.out.println(userInfo);
 
         request.setAttribute("errorMessage", errorMessage);
         request.setAttribute("message", message);
