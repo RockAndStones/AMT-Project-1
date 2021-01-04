1e912a0abc04254d33c11d20aba4b0a8af97a55a
b3cauda <arthur.becaud@heig-vd.ch>
2021-01-04 15:05:25 +0100

    gamiication facade it tests

--- src/test/java/ch/heigvd/amt/stoneoverflow/application/gamification/GamificationFacadeIT.java
+++ src/test/java/ch/heigvd/amt/stoneoverflow/application/gamification/GamificationFacadeIT.java
@@ -1,18 +1,31 @@
 package ch.heigvd.amt.stoneoverflow.application.gamification;
 
+import ch.heigvd.amt.gamification.ApiCallback;
+import ch.heigvd.amt.gamification.ApiException;
+import ch.heigvd.amt.gamification.api.dto.*;
 import ch.heigvd.amt.stoneoverflow.application.ServiceRegistry;
-import ch.heigvd.amt.stoneoverflow.application.gamification.GamificationFacade;
 import ch.heigvd.amt.stoneoverflow.application.identitymgmt.login.AuthenticatedUserDTO;
+import ch.heigvd.amt.stoneoverflow.application.identitymgmt.login.LoginCommand;
+import ch.heigvd.amt.stoneoverflow.application.identitymgmt.login.LoginFailedException;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 
 import javax.inject.Inject;
-import java.io.File;
+import java.util.List;
+import java.util.Map;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@RunWith(Arquillian.class)
 public class GamificationFacadeIT {
 
     private final static String WARNAME = "arquillian-managed.war";
@@ -39,13 +52,95 @@ public class GamificationFacadeIT {
     }
 
     @Before
-    public void init() {
-        gamificationFacade = new GamificationFacade("unitTests-" +  RandomStringUtils.random(10, true, true));
+    public void init() throws LoginFailedException {
+        System.out.println("init-1"); //todo: debug
+        this.testUser = serviceRegistry.getIdentityManagementFacade().login(LoginCommand.builder()
+                .username("test")
+                .plaintextPassword("test")
+                .build());
+        System.out.println("init-2");//todo: debug
+        this.gamificationFacade = new GamificationFacade("unitTests-" +  RandomStringUtils.random(10, true, true));
+        System.out.println("init-3");//todo: debug
+        this.gamificationFacade.stonerProgressAsync(testUser.getId().asString(), newApiCallbackVoidFailOnFailure());
     }
 
     @Test
-    public void testApi() {
+    public void shouldBeInstantiate() {
+        System.out.println(gamificationFacade);
+        assertTrue(gamificationFacade.isInstantiate());
+    }
 
+    @Test
+    public void shouldAddQuestionAsync() {
+        gamificationFacade.addQuestionAsync(testUser.getId().asString(), newApiCallbackVoidFailOnFailure());
+    }
+
+    @Test
+    public void shouldAddReplyAsync() {
+        gamificationFacade.addReplyAsync(testUser.getId().asString(), newApiCallbackVoidFailOnFailure());
+    }
+
+    @Test
+    public void shouldAddCommentAsync() {
+        gamificationFacade.addCommentAsync(testUser.getId().asString(), newApiCallbackVoidFailOnFailure());
+    }
+
+    @Test
+    public void shouldAddAndRemoveVoteAsync() {
+        gamificationFacade.addVoteAsync(testUser.getId().asString(), newApiCallbackVoidFailOnFailure());
+        gamificationFacade.removeVoteAsync(testUser.getId().asString(), newApiCallbackVoidFailOnFailure());
+    }
+
+    @Test
+    public void shouldProgressAndRegressStonerAsync() {
+        gamificationFacade.stonerProgressAsync(testUser.getId().asString(), newApiCallbackVoidFailOnFailure());
+        gamificationFacade.stonerRegressAsync(testUser.getId().asString(), newApiCallbackVoidFailOnFailure());
+    }
+
+    @Test
+    public void shouldGetRightTestUserInfo() {
+        UserInfo testUserInfo = gamificationFacade.getUserInfo(testUser.getId().asString());
+        assertNotNull(testUserInfo);
+        assertEquals(Integer.valueOf(8), testUserInfo.getPoints());
+        assertNotNull(testUserInfo.getBadges());
+        assertEquals(5, testUserInfo.getBadges().size());
+    }
+
+    @Test
+    public void shouldGetRightBadgesRankings() {
+        PaginatedBadgesRankings badgesRankings = gamificationFacade.getBadgesRankings(0, 10);
+        assertNotNull(badgesRankings);
+        assertNotNull(badgesRankings.getData());
+        assertTrue(badgesRankings.getData().contains(new BadgesRanking()
+                .userId(testUser.getId().asString())
+                .points(8)));
+    }
+
+    @Test
+    public void shouldGetRightPointsRankings() {
+        PaginatedPointsRankings pointsRankings = gamificationFacade.getPointsRankings(0, 10);
+        assertNotNull(pointsRankings);
+        assertNotNull(pointsRankings.getData());
+        assertTrue(pointsRankings.getData().contains(new PointsRanking()
+                .userId(testUser.getId().asString())
+                .points(5d)));
+    }
+
+    private ApiCallback<Void> newApiCallbackVoidFailOnFailure() {
+        return new ApiCallback<Void>() {
+            @Override
+            public void onFailure(ApiException e, int statusCode, Map<String, List<String>> responseHeaders) {
+                fail();
+            }
+            @Override
+            public void onSuccess(Void result, int statusCode, Map<String, List<String>> responseHeaders) {
+                System.out.println("IT test action success!");//todo: debug
+            }
+            @Override
+            public void onUploadProgress(long bytesWritten, long contentLength, boolean done) { }
+            @Override
+            public void onDownloadProgress(long bytesRead, long contentLength, boolean done) { }
+        };
     }
 
 }
