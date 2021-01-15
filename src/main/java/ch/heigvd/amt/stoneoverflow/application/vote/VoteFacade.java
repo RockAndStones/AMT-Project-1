fbb0c5ac61ba5884687aa49ab0270a9b85283a8c
Naludrag <stephane.teixeiracarvalho@heig-vd.ch>
2021-01-15 15:59:28 +0100

    Moved gamfication call from voteSerlvet to voteFacade

--- src/main/java/ch/heigvd/amt/stoneoverflow/application/vote/VoteFacade.java
+++ src/main/java/ch/heigvd/amt/stoneoverflow/application/vote/VoteFacade.java
@@ -1,5 +1,6 @@
 package ch.heigvd.amt.stoneoverflow.application.vote;
 
+import ch.heigvd.amt.stoneoverflow.application.gamification.GamificationFacade;
 import ch.heigvd.amt.stoneoverflow.domain.Id;
 import ch.heigvd.amt.stoneoverflow.domain.UserMessageType;
 import ch.heigvd.amt.stoneoverflow.domain.answer.AnswerId;
@@ -14,9 +15,11 @@ import java.util.Optional;
 
 public class VoteFacade {
     private IVoteRepository voteRepository;
+    GamificationFacade gamificationFacade;
 
-    public VoteFacade(IVoteRepository voteRepository) {
+    public VoteFacade(IVoteRepository voteRepository, GamificationFacade gamificationFacade) {
         this.voteRepository = voteRepository;
+        this.gamificationFacade = gamificationFacade;
     }
 
     public VoteId addVote(AddVoteCommand command) {
@@ -25,6 +28,8 @@ public class VoteFacade {
                 .votedObject(command.getVotedObject())
                 .voteType(command.getVoteType()).build();
         voteRepository.save(vote);
+        gamificationFacade.addVoteAsync(command.getVotedBy().asString(), null);
+        gamificationFacade.stonerProgressAsync(command.getVotedBy().asString(), null);
         return vote.getId();
     }
 
@@ -60,8 +65,10 @@ public class VoteFacade {
         return voteRepository.findNbVotes(id, userMessageType);
     }
 
-    public void remove(VoteId voteId) {
+    public void remove(VoteId voteId, UserId userId) {
         voteRepository.remove(voteId);
+        gamificationFacade.removeVoteAsync(userId.asString(), null);
+        gamificationFacade.stonerRegressAsync(userId.asString(), null);
     }
 
     public void changeVote(UpdateVoteCommand command) {
@@ -70,5 +77,13 @@ public class VoteFacade {
                 .votedBy(command.getVotedBy())
                 .votedObject(command.getVotedObject())
                 .voteType(command.getVoteType()).build());
+        // TODO : Better Solution ?
+        if(command.getVoteType() == Vote.VoteType.UP){
+            gamificationFacade.addVoteAsync(command.getVotedBy().asString(), null);
+            gamificationFacade.stonerProgressAsync(command.getVotedBy().asString(), null);
+        } else if(command.getVoteType() == Vote.VoteType.DOWN){
+            gamificationFacade.removeVoteAsync(command.getVotedBy().asString(), null);
+            gamificationFacade.stonerRegressAsync(command.getVotedBy().asString(), null);
+        }
     }
 }
